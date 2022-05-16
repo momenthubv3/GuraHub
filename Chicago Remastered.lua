c6da518777ac6afadffeb564a8384c0cc0ab8758
lobox920 <86623018+lobox920@users.noreply.github.com>
2022-05-16 17:05:55 +0000

    Update Chicago Remastered.lua

--- Chicago Remastered.lua
+++ Chicago Remastered.lua
@@ -1178,25 +1178,23 @@ Tab:AddButton({
 	Callback = function()
 		local delay_hook = nil 
 		delay_hook = replaceclosure(delay, newcclosure(function(...)
-			local Arguments = ({...});
+			local Arguments = ({...})
    
    if getcallingscript() and getcallingscript().Name ~= "GunClient" then 
-       return delay_hook(...);
+       return delay_hook(...)
    end
    
    table.foreach(Arguments, function(Index, Value)
        if Value and type(Value) == "function" then 
-           local valid_constants = getconstants(Value);
-           -- I know I can just do getconstants(Arguments[2]) but idk they might change it 
-           
+           local valid_constants = getconstants(Value)
            if valid_constants and table.find(valid_constants, "CoordinateFrame") then 
                setconstant(Value, 12, "focus")
            end
        end
    end)
    
-   return delay_hook(...);
-end));
+   return delay_hook(...)
+end))
 
 	    
 	end    
