b0bf57f73355db085bbec417669631295fd26d92
lobox920 <86623018+lobox920@users.noreply.github.com>
2022-06-14 01:16:34 +0000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,6 +2,8 @@ if game.PlaceId == 7167319176 then
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/lobox920/Shar-Script/main/Chicago%20Remastered.lua')))()
 elseif game.PlaceId == 8425637426 then
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/lobox920/Shar-Script/main/Write%20a%20Letter.lua')))()
+elseif game.PlaceId == 6728870912 then
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/lobox920/Shar-Script/main/World%20of%20Stands.lua')))()')))()
 else
     local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
     OrionLib:MakeNotification({
