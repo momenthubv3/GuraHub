8f311f6625f690a40cbbc0f2910a23c2d1207ae1
lobox920 <86623018+lobox920@users.noreply.github.com>
2022-06-16 19:27:43 +0000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,6 +4,8 @@ elseif game.PlaceId == 8425637426 then
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/lobox920/Shar-Script/main/Write%20a%20Letter.lua')))()
 elseif game.PlaceId == 6728870912 then
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/lobox920/Shar-Script/main/World%20of%20Stands.lua')))()
+elseif game.PlaceId == 5910449407 then
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/lobox920/Shar-Script/main/Project%20Menacing.lua')))()
 else
     local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
     OrionLib:MakeNotification({
